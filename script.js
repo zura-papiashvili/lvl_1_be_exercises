669f431d5708823fd278e8060c2646b9178d8f44
Giorgi Andriadze <g.andriadze2@gmail.com>
2021-03-15 19:59:38 +0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+let firstVar = 'rame';
+console.log(firstVar);
+let secondVar = 'meore';
+let thirdVar = '';
+console.log(firstVar);
+
+
+// firtVar = 'rame'
+// thirdVar = ''
+// secondVar = 'meore'
+thirdVar = firstVar;
+
+
+// firtVar = 'meore'
+// thirdVar = 'rame'
+// secondVar = 'meore'
+firstVar = secondVar;
+
+// firtVar = 'meore'
+// thirdVar = 'rame'
+// secondVar = 'meore'
+secondVar = thirdVar;
+
+
+console.log('First: ', firstVar);
+console.log('Second:', secondVar);
